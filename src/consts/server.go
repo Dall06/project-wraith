ba3f3f74ab98b348209f0e9be341e151dbb49dc2
Diego A. León <diego.leon@jaak.ai>
2024-09-24 23:35:29 -0600

    feat: add changes

--- src/consts/server.go
+++ src/consts/server.go
@@ -2,6 +2,5 @@ package consts
 
 const (
 	LicenseCheck      = false // Change to false if you don't want to check for licenses during development
-	StaticsPath       = "/public/scripts"
 	LoggerCallerLevel = 2
 )
